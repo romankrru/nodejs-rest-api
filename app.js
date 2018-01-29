e3700980311c3e10f55c36bceed5bc32d03b7928
Roman <rm07ru@gmail.com>
2018-01-29 18:25:25 +0300

    Add routes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!',
-  });
-});
+const productsRoute = require('./api/routes/products');
+const ordersRoute = require('./api/routes/orders');
+
+app.use('/products', productsRoute);
+app.use('/order', ordersRoute);
 
 module.exports = app;
