2402ee56d3463361aef9d3c38abcd52e62e00282
Roman <rm07ru@gmail.com>
2018-01-29 19:10:21 +0300

    Add CORS handling

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// CORS handling
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
+// routes
 app.use('/products', productsRoute);
 app.use('/orders', ordersRoute);
 
