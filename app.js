d1e84691ca09bbe629c09122fb32c2c554813607
Roman <rm07ru@gmail.com>
2018-01-29 18:41:28 +0300

    Add error handling

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ app.use(morgan('dev'));
 app.use('/products', productsRoute);
 app.use('/order', ordersRoute);
 
+// error handling
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
 module.exports = app;
