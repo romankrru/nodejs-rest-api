534fda26c834baae35ff64b5f5eb62ded97f4fa1
Roman <rm07ru@gmail.com>
2018-01-29 18:34:03 +0300

    Add morgan

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 const productsRoute = require('./api/routes/products');
 const ordersRoute = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productsRoute);
 app.use('/order', ordersRoute);
